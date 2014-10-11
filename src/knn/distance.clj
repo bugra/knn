c933dbd1ef29a2fe61a48d23b174344445efac3d
Bugra Akyildiz <bugraa@live.com>
2014-10-11 12:22:32 -0400

    Better documentation for distance functions

--- src/knn/distance.clj
+++ src/knn/distance.clj
@@ -9,19 +9,19 @@
   (reduce + (map #(Math/abs (+ %1 %2)) first-vector second-vector)))
 
 (defn manhattan-distance
-  "Manhattan Distance between two vectors, called l_1 distance as well
+  "Manhattan Distance between two vectors, called l_1 distance as well\n
   Formula: sum(abs(x - y))"
   [first-vector second-vector]
   (reduce + (map #(Math/abs (- %1 %2)) first-vector second-vector)))
 
 (defn euclidean-distance
-  "Euclidean distance between two vectors
+  "Euclidean distance between two vectors\n
   Formula: sqrt(sum((x - y) .^ 2))"
   [first-vector second-vector]
   (Math/sqrt (reduce + (map #(Math/pow (- %1 %2) 2) first-vector second-vector))))
 
 (defn squared-euclidean-distance
-  "Squared Euclidean Distance between two vectors
+  "Squared Euclidean Distance between two vectors\n
   Formula: sum((x - y) .^ 2)"
   [first-vector second-vector]
   (* (euclidean-distance first-vector second-vector) (euclidean-distance first-vector second-vector)))
@@ -29,14 +29,14 @@
 (defn minkowski-distance
   "Minkowski Distance, known as l-p distance
   Most Generic form of l-p distances, covers l_1(manhattan)
-  and l_2(euclidean distances) as well
+  and l_2(euclidean distances) as well\n
   Formula: sum(abs(x - y).^p) ^ (1/p)"
   [first-vector second-vector p]
   (Math/pow (reduce + (map #(Math/pow (Math/abs (- %1 %2)) p) first-vector second-vector)) (/ p)))
 
 (defn weighted-minkowski-distance
   "Weighted Minkowski Distance, with different weights
-  on the difference between observations
+  on the difference between observations\n
   Formula: sum(abs(x - y).^p .* w) ^ (1/p)"
   [first-vector second-vector weight-vector p]
   (Math/pow
@@ -45,13 +45,13 @@
 
 (defn chebyshev-distance
   "Chebyshev Distance
-  It is a form of l_p distance when p -> /Infinity
+  It is a form of l_p distance when p -> /Infinity\n
   Formula: max(abs(x - y))"
   [first-vector second-vector]
   (apply max (map #(Math/abs (- %1 %2)) first-vector second-vector)))
 
 (defn hamming-distance
-  "Hamming Distance between two vectors
+  "Hamming Distance between two vectors\n
   Formula: sum(x .!= y)"
   [first-vector second-vector]
   (count
@@ -68,20 +68,20 @@
   (reduce + (map #(/ (Math/abs (- %1 %2)) (+ (Math/abs %1) (Math/abs %2))) first-vector second-vector)))
 
 (defn cosine-distance
-  "Cosine distance between two vectors
+  "Cosine distance between two vectors\n
   Formula: 1 - dot(x, y) / (norm(x) * norm(y))"
   [first-vector second-vector]
   (- 1 (/ (op/dot first-vector second-vector) (* (norm/euclidean-norm first-vector) (norm/euclidean-norm second-vector) ))))
 
 (defn correlation-distance
-  "Correlation distance between two vectors
+  "Correlation distance between two vectors\n
   Formula: cosine-distance(x - mean(x), y - mean(y))"
   [first-vector second-vector]
   (cosine-distance (op/scalar-subtract first-vector (op/mean first-vector))
                    (op/scalar-subtract second-vector (op/mean second-vector))))
 
 (defn chi-squared-distance
-  "Chi Squared Distance between two vectors
+  "Chi Squared Distance between two vectors\n
   Formula: sum((x - y).^2 / (x + y))"
   [first-vector second-vector]
   (reduce + (map #(/ (* (- %1 %2) (- %1 %2) ) (+ %1 %2)) first-vector second-vector)))
@@ -93,13 +93,13 @@
   (reduce + (op/mult first-vector (map func (op/div first-vector second-vector)))))
 
 (defn kl-divergence
-  "Kullback-Leibler Divergence with a normal Log function
+  "Kullback-Leibler Divergence with a normal Log function\n
   Formula: sum(p .* log(p ./ q))"
   [first-vector second-vector]
   (kl-div first-vector second-vector #(Math/log %)))
 
 (defn js-divergence
-  "JS Divergence of two vectors
+  "JS Divergence of two vectors\n
   Formula: KL(x, m) / 2 + KL(y, m) / 2 where
   m = (x + y) / 2"
   [first-vector second-vector]
@@ -107,13 +107,13 @@
     (+ (kl-divergence first-vector m) (kl-divergence second-vector m))))
 
 (defn span-norm-distance
-  "Span-norm distance between two vectors
+  "Span-norm distance between two vectors\n
   Formula: max(x - y) - min(x - y)"
   [first-vector second-vector]
   (- (apply max (op/subtract first-vector second-vector)) (apply min (op/subtract first-vector second-vector))))
 
 (defn bhattacharyya-distance
-  "Bhattacharyya Distance between two vectors
+  "Bhattacharyya Distance between two vectors\n
   Formula: -log(sum(sqrt(x .* y) / sqrt(sum(x) * sum(y)))"
   [first-vector second-vector]
   (- (Math/log
