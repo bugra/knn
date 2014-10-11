7a8c506b8543403cd86ece06dfa7622481da8b75
Bugra Akyildiz <bugraa@live.com>
2014-10-11 12:23:29 -0400

    Get rid of snapshot

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject knn "0.1.0-SNAPSHOT"
+(defproject knn "0.1.0"
   :description "knn: K-Nearests Nieghbor Classifier"
   :url "https://github.com/bugra/knn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main knn.core
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:plugins [[lein-marginalia "0.8.0"]]}})
+  :profiles {:dev {:plugins [[lein-marginalia "0.8.0"]]}}); for documentation
