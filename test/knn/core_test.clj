2387e2f1a3e750b9a67f432fd7d5b8475059a538
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2014-12-05 00:13:46 +0000

    weighted knn included

--- test/knn/core_test.clj
+++ test/knn/core_test.clj
@@ -1,9 +1,29 @@
 (ns knn.core-test
   (:require [clojure.test :refer :all]
-            [knn.core :refer :all]))
+            [knn.core :refer :all]
+            [knn.distance :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
-
+(deftest test-knn
+  (testing "Tests weighted version of kNN."
+    (is (= (score-labels [(struct observation "1" [1 2])
+                         (struct observation "1" [2 3])
+                         (struct observation "-1" [2 3])]
+                         [(struct observation "1" [9 0])]
+                         euclidean-distance 1)
+           [{"1" 7.615773105863909}]))
+    (is (= (score-labels [(struct observation "1" [1 2])
+                         (struct observation "1" [2 3])
+                         (struct observation "-1" [2 3])]
+                         [(struct observation "1" [9 0])]
+                         euclidean-distance 3)
+           [{"-1" 7.615773105863909, "1" 15.86198435709923}]))
+    (is (= (score-labels [(struct observation "1" [1 2])
+                         (struct observation "1" [2 3])
+                         (struct observation "-1" [2 3])]
+                         [(struct observation "1" [9 0])]
+                         cosine-distance 3 :score-modifier (partial - 1))
+            [{"-1" 0.5547001962252291, "1" 1.001913791725187}]))))
