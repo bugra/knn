0cff1f61e5eb0820fe2f8d69dcd1d28d045f0214
Thiago Galery <tgalery@gmail.com>
2014-12-12 16:33:22 +0000

    making test more informative

--- test/knn/similarity_test.clj
+++ test/knn/similarity_test.clj
@@ -10,9 +10,6 @@
 (def first-vector [4. 5. 6. 7.])
 (def second-vector [3. 9. 8. 1.])
 
-(def third-vector [1. 2. 1. 3. 2. 1.])
-(def fourth-vector [1. 3. 0. 2. 2. 0.])
-
 (defn round
   "Round double/float numbers upto 2 decimal places"
   [number]
@@ -23,4 +20,4 @@
     (is (= 0.8 (round (cosine-similarity first-vector second-vector))))
     (is (= 1.0 (cosine-similarity first-vector first-vector)))
     (is (= 1.0 (cosine-similarity second-vector second-vector))))
-    (is (= 1.0 (cosine-similarity zero-vector zero-vector))))
+    (is (thrown? ArithmeticException (cosine-similarity zero-vector first-vector))))
